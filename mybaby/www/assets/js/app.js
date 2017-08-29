e554986a1fc46cd6ae840bf504b86702c2fef89e
Gábor <lizergsav@gmail.com>
2017-08-29 23:31:23 +0200

    next commit only for sure

---
+++ mybaby/www/assets/js/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#content',
+  data: {
+    message: 'This is my text',
+    babyWeight:[
+      {id:1, startWeight:3550,endWeight:3560, createdTime:'2017-08-24:11h22'},
+      {id:2,startWeight:3530,endWeight:3570, createdTime:'2017-08-24:11h22'},
+      {id:3,startWeight:3520,endWeight:3580, createdTime:'2017-08-24:11h22'},
+      {id:4,startWeight:3570,endWeight:3660, createdTime:'2017-08-24:11h22'}
+    ]
+  }
+})
